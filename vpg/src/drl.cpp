5e26e7e675789536a679c0c30189a2c7b6347c95
khersham <khersham@hotmail.com>
2020-06-01 18:36:35 +0200

    formatted vpg

--- vpg/src/drl.cpp
+++ vpg/src/drl.cpp
@@ -2,39 +2,15 @@
 
 #include "drl.h"
 
-/**
-namespace torch{
-//PyTorch does not generate this constructor by default
-torch::Tensor normal(const torch::Tensor &mean, 
-                     const torch::Tensor &std, 
-                     torch::Generator *generator = nullptr) {
-  return at::normal(mean, std, generator);  
-}
-}
-**/
-
 namespace drl {
 
-/**
-//log_prob function 
-torch::Tensor log_prob(const torch::Tensor& mean, const torch::Tensor& scale, const torch::Tensor& value) {
-    auto std = torch::exp(scale);
-    auto var = std * std;
-    auto logscale = std.log();
-    auto nom = (mean - value)*(value - mean);
-    auto constant = log(sqrt(2 * M_PI) );
-    return nom / (2 * var) -logscale - constant;
-}
+VPGBuffer::VPGBuffer(const int64_t& obs_dim_,
+                     const int64_t& act_dim_, 
+                     const int64_t& max_size_, 
+                     const float& gamma_, 
+                     const float& lam_):
+    gamma{gamma_}, lam{lam_}, max_size{max_size_} {
 
-torch::Tensor sample(const torch::Tensor& mean, const torch::Tensor& scale) {
-    torch::NoGradGuard no_grad;
-    return torch::normal(mean, torch::exp(scale));
-}
-**/
-
-VPGBuffer::VPGBuffer(const int64_t& obs_dim_, const int64_t& act_dim_, const int64_t& max_size_, 
-                     const float& gamma_, const float& lam_):
-gamma{gamma_}, lam{lam_}, max_size{max_size_} {
     obs_buf = torch::zeros({max_size, obs_dim_}, options);
     act_buf = torch::zeros({max_size, act_dim_}, options); 
     adv_buf = torch::zeros(max_size, options); 
@@ -45,6 +21,7 @@ gamma{gamma_}, lam{lam_}, max_size{max_size_} {
 }
 
 torch::Tensor VPGBuffer::discount(torch::Tensor xarray, const float& gamma_){
+
     auto shape = xarray.sizes()[0];
     torch::Tensor yarray = torch::zeros(shape);
     for (int64_t i=0; i != shape; ++i){
@@ -58,8 +35,12 @@ torch::Tensor VPGBuffer::discount(torch::Tensor xarray, const float& gamma_){
     return yarray;
 }
    
-void VPGBuffer::store(const torch::Tensor& obs, const torch::Tensor& act, const torch::Tensor& rew,
-                      const torch::Tensor& val, const torch::Tensor& logp) {
+void VPGBuffer::store(const torch::Tensor& obs, 
+                      const torch::Tensor& act, 
+                      const torch::Tensor& rew,
+                      const torch::Tensor& val, 
+                      const torch::Tensor& logp) {
+
     if (ptr < max_size) {
         obs_buf[ptr] = obs;
         act_buf[ptr] = act;
@@ -85,7 +66,12 @@ void VPGBuffer::finish_path(const torch::Tensor& last_val){
     ptr_start_idx = ptr;
 }
 
-std::tuple<torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor> VPGBuffer::get() {
+std::tuple<torch::Tensor, 
+           torch::Tensor, 
+           torch::Tensor, 
+           torch::Tensor, 
+           torch::Tensor> VPGBuffer::get() {
+
     assert(ptr == max_size);
     ptr = 0;
     ptr_start_idx = 0;
@@ -95,15 +81,20 @@ std::tuple<torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Te
     return {obs_buf, act_buf, ret_buf, adv_buf, logp_buf};
 }
 
-torch::Tensor compute_loss_v(const torch::Tensor& obs, const torch::Tensor& ret, ActorCritic& ac){
+torch::Tensor compute_loss_v(const torch::Tensor& obs, 
+                             const torch::Tensor& ret, 
+                             ActorCritic& ac) {
+
     auto v = (ac->critic_)->forward(obs);
     auto loss_v = ((v - ret).pow(2)).mean();
-    //Policy loss
+    //v loss
     return loss_v;
 }
 
-torch::Tensor compute_loss_pi(const torch::Tensor& obs, const torch::Tensor& act, 
-                              const torch::Tensor& adv, ActorCritic& ac){
+torch::Tensor compute_loss_pi(const torch::Tensor& obs, 
+                              const torch::Tensor& act, 
+                              const torch::Tensor& adv, 
+                              ActorCritic& ac){
 
     auto [pi, logp] = (ac->actor_)->forward(obs, act);
     auto loss_pi = -(logp * adv).mean();
@@ -111,8 +102,12 @@ torch::Tensor compute_loss_pi(const torch::Tensor& obs, const torch::Tensor& act
     return loss_pi;
 }
 
-void update(VPGBuffer& buf, ActorCritic& ac, torch::optim::Adam& pi_optimizer, 
-            torch::optim::Adam& v_optimizer, const int& critic_iter, const torch::Device& device) {
+void update(VPGBuffer& buf, ActorCritic& ac, 
+            torch::optim::Adam& pi_optimizer, 
+            torch::optim::Adam& v_optimizer, 
+            const int& critic_iter, 
+            const torch::Device& device) {
+
     auto [obs, act, ret, adv, logp] = buf.get();
     obs = obs.to(device);
     act = act.to(device);
